8702994b3c3c05e0a1cfb47bb626933e33f03c54
Ultrasteve <609117264@qq.com>
2017-06-04 22:15:16 +0800

    2017-06-03

--- js/flower.js
+++ js/flower.js
@@ -15,3 +15,41 @@ var showDropdown = function() {
 	else
 		onShow[0].style.display = "block";
 }
+
+var reEdit = function() {
+		var onNone = document.getElementsByClassName('complete-editBooker');
+		var onShow = document.getElementsByClassName('editBooker');
+		onNone[0].style.display = "none";
+		onShow[0].style.display = "inline-block";
+}
+var completeEdit = function() {
+	var onShow = document.getElementsByClassName('complete-editBooker');
+	var onNone = document.getElementsByClassName('editBooker');
+	//添加结果
+
+	var result = document.getElementsByClassName('result');
+	var editResult = document.getElementsByClassName('editResult');
+	for(let i = 0;i<result.length;i++) {
+		editResult[i].innerHTML = result[i].value;
+	}
+
+	onNone[0].style.display = "none";
+	onShow[0].style.display = "inline-block";
+}
+var openAlert = function() {
+	//if()
+	var onOperate = document.getElementsByClassName("alert-box");
+	onOperate[0].style.display = "block";
+}
+var closeAlert = function() {
+	//if()
+	var onOperate = document.getElementsByClassName("alert-box");
+	onOperate[0].style.display = "none";
+}
+var submitInfo = function() {
+	document.getElementById("addBooker").style.display = "none";
+	
+}
+var editInfo = function() {
+	openAlert();
+}
